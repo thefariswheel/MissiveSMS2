0e8817731ff9812f956c76d32b9f549c7f8c2281
faristhewheel <Faris@Fariss-iMac.attlocal.net>
2016-07-22 14:31:52 -0700

    Tab Bar design with UI implementations

---
+++ app/src/main/java/missivecommunications/missivesms/Tab1.java
@@ -0,0 +1,25 @@
+package missivecommunications.missivesms;
+
+/**
+ * Created by Faris on 7/22/16.
+ */
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+    /**
+     * Created by hp1 on 21-01-2015.
+     */
+    public class Tab1 extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View v =inflater.inflate(R.layout.tab_1,container,false);
+            return v;
+        }
+    }
